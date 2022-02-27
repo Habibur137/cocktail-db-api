6fc8b66f511e56aa48cf8e1837534d882e141dd4
Habibur137 <infernohabib@gmail.com>
2022-02-27 19:33:33 +0600

    single drinks item details shown

--- cocktail.js
+++ cocktail.js
@@ -15,7 +15,28 @@ const displayCocktailData = (cocktails) => {
             <img src="${cocktail.strDrinkThumb}" />
             <h3>${cocktail.strDrink}</h3>
             <p>${cocktail.strInstructions}</p>
+            <button onclick="singleCocktailDetail('${cocktail.idDrink}')">Drinks Detail</button>
         `
         root.append(div)
     });
+}
+
+const singleCocktailDetail = (cocktailId) => {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`)
+    .then(res => res.json())
+    .then(data => displaySingleCocktail(data.drinks[0]))
+}
+
+const displaySingleCocktail = (cocktail) => {
+    console.log(cocktail)
+    const item = document.getElementById('item')
+    item.textContent = ''
+    const div = document.createElement('div')
+    div.className = 'cocktail-item'
+    div.innerHTML = `
+        <img src="${cocktail.strDrinkThumb}" />
+        <h3>${cocktail.strDrink}</h3>
+        <p>${cocktail.strInstructions}</p>
+    `
+    item.append(div)
 }
