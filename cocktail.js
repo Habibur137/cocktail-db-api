f0b7b6e52d92dc62f0651c6a56f4f64752d00beb
Habibur137 <infernohabib@gmail.com>
2022-02-27 18:47:54 +0600

    first commit

---
+++ cocktail.js
@@ -0,0 +1,12 @@
+const loadCocktailData = () => {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`)
+    .then(res => res.json())
+    .then(data => displayCocktailData(data.drinks))
+}
+loadCocktailData()
+
+const displayCocktailData = (cocktails) => {
+    cocktails.forEach(cocktail => {
+        console.log(cocktail)
+    });
+}
